4e06da6ea2c9400cf691cd1d763fa66268d40bed
amarilisdev <amarilismoldes.dev@gmail.com>
2025-08-19 13:58:13 +0200

    cache

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 
@@ -16,12 +18,16 @@ class MainController extends AbstractController
     public function homepage(
         StarshipRepository $starshipRepository,
         HttpClientInterface $client,
+        CacheInterface $issLocationPool,
     ): Response {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-        $issData = $response->toArray();
+        $issData = $issLocationPool->get('iss_location_data', function (ItemInterface $item) use ($client): array {
+            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+            
+            return $response->toArray();
+        });
 
 
 
